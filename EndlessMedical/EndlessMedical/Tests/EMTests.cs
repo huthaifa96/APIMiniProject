2c2c1ac8847cb0b3bef40efcbd5241da0fcf6a8c
Vinay Patel <vinay.light@hotmail.co.uk>
2020-09-25 14:01:19 +0100

    sessionid tested

--- EndlessMedical/EndlessMedical/Tests/EMTests.cs
+++ EndlessMedical/EndlessMedical/Tests/EMTests.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using NUnit.Framework;
 
 
 namespace EndlessMedical
 {
-    public class UnitTest1
+    public class EMTests
     {
+        private EMService _EMService = new EMService();
+
+        [Test]
+        public void CheckStatusIs200()
+        {
+            Assert.That(_EMService.SessionID != null);
+        }
+
 
     }
 }
